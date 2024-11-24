00666db671a2a3c94bc4c5a9e6bc418778d7a12c
Amaan Ansari <amaan0076@gmail.com>
2024-11-24 08:59:40 +0530

    Routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentUserPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatarImage,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,4 +37,21 @@ router.post("/login", loginUser);
 //Secured Routes
 router.post("/logout", verifyJWT, logoutUser);
 router.post("/refresh-token", refreshAccessToken);
+router.post("/change-password", verifyJWT, changeCurrentUserPassword);
+router.get("/current-user", verifyJWT, getCurrentUser);
+router.patch("/update-account", verifyJWT, updateAccountDetails);
+router.patch(
+  "/update-avatar",
+  verifyJWT,
+  upload.single("avatar"),
+  updateAvatarImage
+);
+router.patch(
+  "/cover-image-update",
+  verifyJWT,
+  upload.single("coverImage"),
+  updateCoverImage
+);
+router.get("/channel/:username", verifyJWT, getUserChannelProfile);
+router.get("/watch-history", verifyJWT, getWatchHistory);
 export default router;
